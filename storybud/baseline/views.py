d98d97c35d1a3857830d3fe9b974d35a9f3fc0ce
Bruno Quiroga <bquiroga@fi.uba.ar>
2024-01-03 16:04:45 -0300

    database, models y admin, un poco de UI

--- storybud/baseline/views.py
+++ storybud/baseline/views.py
@@ -1,24 +1,22 @@
 from django.shortcuts import render
-
+from .models import Room
+'''
 rooms = [
     {'id': 1, 'name': 'Room 1', 'description': 'This is a room for COVID-19 patients'},
     {'id': 2, 'name': 'Room 2', 'description': 'This is a room for patients with other illnesses'},
     {'id': 3, 'name': 'Room 3', 'description': 'This is a room for kids'},
 
-]
+]'''
 
 
 
 def home(request):
+    rooms = Room.objects.all()
     return render(request, 'baseline/home.html', {'rooms': rooms})
 
-def room(request, pk=None):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-            break
-
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    
     context = {
         'room': room
     }
