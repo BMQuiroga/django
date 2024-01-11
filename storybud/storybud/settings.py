cb754cbb17cba36f166242fe89618ac405292a91
Bruno Quiroga <bquiroga@fi.uba.ar>
2024-01-11 19:04:29 -0300

    CORS POLICY
    625

--- storybud/storybud/settings.py
+++ storybud/storybud/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'baseline.apps.BaselineConfig',
 
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware', #new
 ]
 
 ROOT_URLCONF = 'storybud.urls'
@@ -130,3 +133,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True #new
