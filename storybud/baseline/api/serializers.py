e339f1a23e980ec0a61245f3fd8129ed63602887
Bruno Quiroga <bquiroga@fi.uba.ar>
2024-01-18 21:13:09 -0300

    mas endpoints

--- storybud/baseline/api/serializers.py
+++ storybud/baseline/api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework.serializers import ModelSerializer
-from baseline.models import Room
+from baseline.models import Room, User, Message
 
 class RoomSerializer(ModelSerializer):
     class Meta:
         model = Room
+        fields = '__all__'
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'name', 'avatar', 'bio']
+
+class MessageSerializer(ModelSerializer):
+    class Meta:
+        model = Message
         fields = '__all__'
--- storybud/baseline/api/views.py
+++ storybud/baseline/api/views.py
@@ -1,8 +1,8 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from baseline.models import Room
-from .serializers import RoomSerializer
+from baseline.models import Room, User, Message
+from .serializers import RoomSerializer, UserSerializer, MessageSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -10,6 +10,10 @@ def getRoutes(request):
         'GET /api/',
         'GET /api/rooms',
         'GET /api/rooms/:id',
+        'GET /api/rooms/:id/messages',
+        'GET /api/users',
+        'GET /api/users/:id',
+
     ]
     return Response(routes)#permite que las rutas usen data json en vez de diccionarios de py
 
@@ -26,3 +30,25 @@ def getRoom(request, pk):
     #no puede mandar una lista de python, se usan serializers
     serializer = RoomSerializer(room, many=False)# many es para que sepa que es una lista (muchos objetos)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def getUsers(request):
+    users = User.objects.all()
+
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getUser(request, pk):
+    user = User.objects.get(id=pk)
+
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getMessages(request, pk):
+    messages = Room.objects.get(id=pk).message_set.all()
+
+    serializer = MessageSerializer(messages, many=True)
+    return Response(serializer.data)
+
